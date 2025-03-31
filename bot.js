3298d864c555f1f0389d4defc98a7d1a6dcb7d66
VitalikSlavik <vitalii.chycherskyi@whykingz.com>
2025-03-31 21:38:29 +0300

    asd

--- bot.js
+++ bot.js
@@ -5,9 +5,8 @@
     const bot = new Telegraf(process.env.BOT_TOKEN);
 
     // Обробник повідомлень
-    bot.on('text', (ctx) => {
-        ctx.reply('Hello!');
-    });
+    console.log(`Chat ID: ${ctx.chat.id}`);
+    ctx.reply(`Ваш chat_id: ${ctx.chat.id}`);
 
     // Запуск бота
     bot.launch();
